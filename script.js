edd192e6b6d796aa0c0ddcc08e62c6fc10c5162d
Kelly M Collins <90kcol@gmail.com>
2019-12-02 15:14:54 -0500

    Created function to determine winner

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 console.log('I work');
 
+let aNodeList = document.querySelectorAll('.square')
+let a = Array.from(aNodeList)
+function getWinner(){
+  if ((a[0].innerHTML === a[1].innerHTML && a[1].innerHTML === a[2].innerHTML)||(a[0].innerHTML === a[3].innerHTML && a[3].innerHTML === a[6].innerHTML)||(a[1].innerHTML === a[4].innerHTML && a[4].innerHTML === a[7].innerHTML)||(a[2].innerHTML === a[5].innerHTML && a[5].innerHTML === a[8].innerHTML)||(a[3].innerHTML === a[4].innerHTML && a[4].innerHTML === a[5].innerHTML)||(a[6].innerHTML === a[7].innerHTML && a[7].innerHTML === a[8].innerHTML)||(a[0].innerHTML === a[4].innerHTML && a[4].innerHTML === a[8].innerHTML)||(a[2].innerHTML === a[4].innerHTML && a[4].innerHTML === a[6].innerHTML)) {
+    alert(document.querySelector('h2').innerHTML + ' wins!!')
+
+  } else {
+    console.log('keep playing')
+  }
+}
+
 function playGame(){
   for (let i = 0; i < 9; i++) {
-    let a = document.querySelectorAll('.square')
     a[i].addEventListener('click', function (evt){
       evt.preventDefault();
       if (document.querySelector('h2').innerHTML === 'Player 2') {
         evt.target.style.backgroundColor = 'red';
-        evt.target.style.pointerEvents = 'none'
+        evt.target.style.pointerEvents = 'none';
+        evt.target.innerHTML = 'x';
+        getWinner()
         document.querySelector('h2').innerHTML = 'Player 1'
         document.querySelector('p').innerHTML = 'Blue'
         } else if (document.querySelector('h2').innerHTML === 'Player 1') {
           evt.target.style.backgroundColor = 'blue';
-          evt.target.style.pointerEvents = 'none'
+          evt.target.style.pointerEvents = 'none';
+          evt.target.innerHTML = 'o';
+          getWinner()
           document.querySelector('h2').innerHTML = 'Player 2'
           document.querySelector('p').innerHTML = 'Red'
         }
